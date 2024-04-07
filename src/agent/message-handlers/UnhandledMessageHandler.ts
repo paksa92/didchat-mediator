a1e379b4f4019cce93daa0407e9460a8b6303327
Sharif Paksa <s.paksa@protonmail.com>
2024-04-07 23:25:09 +0000

    prebuild, sse

--- src/agent/message-handlers/UnhandledMessageHandler.ts
+++ src/agent/message-handlers/UnhandledMessageHandler.ts
@@ -1,9 +1,23 @@
+import { IAgentContext, TAgent } from "@veramo/core";
 import { Message, AbstractMessageHandler } from "@veramo/message-handler";
 
+import { DIDChatMediator } from "../setup";
+
 export class UnhandledMessageHandler extends AbstractMessageHandler {
-  async handle(message: Message): Promise<Message> {
+  public supportedMessageTypes = [
+    "https://didcomm.org/messagepickup/3.0/messages-received",
+  ];
+
+  async handle(
+    message: Message,
+    context: IAgentContext<TAgent<DIDChatMediator>>
+  ): Promise<Message> {
+    if (this.supportedMessageTypes.includes(message.type)) {
+      return message;
+    }
+
     console.log("RECEIVED UNHANDLED MESSAGE", JSON.stringify(message, null, 2));
 
-    return message;
+    return super.handle(message, context);
   }
 }
